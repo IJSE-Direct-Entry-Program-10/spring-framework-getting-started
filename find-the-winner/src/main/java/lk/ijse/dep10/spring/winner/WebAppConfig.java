543b230f5486e30b105954e0485fdac33a4bce78
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2023-05-09 17:02:00 +0530

    Finalize the home page style - part 1

---
+++ find-the-winner/src/main/java/lk/ijse/dep10/spring/winner/WebAppConfig.java
@@ -0,0 +1,9 @@
+package lk.ijse.dep10.spring.winner;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@EnableWebMvc
+@Configuration
+public class WebAppConfig {
+}
